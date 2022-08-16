dbfcd7283f0164fc979e3e821d57b3d6dabdf25a
Enzo Holgado <enzoholgadodev@gmail.com>
2022-08-16 10:27:11 -0500

    imports dependencies

--- src/components/FilterNav.jsx
+++ src/components/FilterNav.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector, filterOnly } from "react-redux";
 import { filter, order, reset } from "../redux/actions";
 
 const FilterNav = () => {
